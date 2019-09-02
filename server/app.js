fadc93133f1408b9e9958e24aa3b1a8d75395234
venki0086 <venkatesh.chavvakula@cognitiveinnovations.in>
2019-09-02 14:48:25 +0530

    add complete backend

---
+++ server/app.js
@@ -0,0 +1,37 @@
+/**
+ * Main application file
+ */
+
+'use strict';
+
+// Set default node environment to development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var express = require('express');
+var mongoose = require('mongoose');
+var config = require('./config/environment');
+var cors = require('cors');
+// Connect to database
+mongoose.connect(config.mongo.uri, config.mongo.options);
+mongoose.connection.on('error', function(err) {
+	console.error('MongoDB connection error: ' + err);
+	process.exit(-1);
+	}
+);
+// Populate DB with sample data
+if(config.seedDB) { require('./config/seed'); }
+
+// Setup server
+var app = express();
+app.use(cors());
+var server = require('http').createServer(app);
+require('./config/express')(app);
+require('./routes')(app);
+
+// Start server
+server.listen(config.port, config.ip, function () {
+  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+});
+
+// Expose app
+exports = module.exports = app;
---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://venki519:venki519@ds161285.mlab.com:61285/chatserver'
+  },
+
+  seedDB: true
+};
